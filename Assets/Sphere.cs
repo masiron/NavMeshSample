e81f1b9330e0ac6c45a9719d21fec9b4a1304b85
加藤翼 <masiron3939@gmail.com>
2017-05-21 17:35:10 +0900

    create NavMesh Sample

---
+++ Assets/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Sphere : MonoBehaviour
+{
+	[SerializeField] private GameObject target;
+	// Use this for initialization
+	void Start () 
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.speed = 10;
+		agent.destination = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
